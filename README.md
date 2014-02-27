e62fb173246d3f825d33908b11f4772c2428c284
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2014-02-27 09:10:30 +0100

    Refactoring

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 circleci-badges
 ===============
 
-CircleCI badges make it right (with Shield)
+CircleCI badges make right (with [shields.io](http://shields.io))
+
+## Usage
+
+Just call `https://circleci-badges.herokuapp.com/:owner/:repo/:circle-ci-badge-token`
--- web.rb
+++ web.rb
@@ -2,35 +2,10 @@ require 'sinatra'
 require 'open-uri'
 require 'multi_xml'
 
-class CircleCI
-  attr_accessor :owner, :repo, :token
-
-  def initialize(owner, repo, token)
-    @owner = owner
-    @repo = repo
-    @token = token
-  end
-
-  def status
-    xml = open(_url).read
-    data = MultiXml.parse(xml)
-    p data
-    data['Projects']['Project']['lastBuildStatus']
-  end
-
-  private
-
-  def _url
-    "https://circleci.com/gh/#{owner}/#{repo}.cc.xml?circle-token=#{token}"
-  end
-
-end
-
 get '/:owner/:repo/:token' do
-  circleci = CircleCI.new(params[:owner], params[:repo], params[:token])
-  if circleci.status == 'Success'
-    redirect 'https://img.shields.io/badge/build-passing-green.svg'
-  else
-    redirect 'https://img.shields.io/badge/build-failing-red.svg'
-  end
+  url = "https://circleci.com/gh/#{params[:owner]}/#{params[:repo]}.cc.xml?circle-token=#{params[:token]}"
+  xml = open(url).read
+  status = MultiXml.parse(xml)['Projects']['Project']['lastBuildStatus']
+  badge = status == 'Success' ? 'build-passing-green.svg' : 'build-failing-red.svg'
+  redirect "https://img.shields.io/badge/#{badge}"
 end
