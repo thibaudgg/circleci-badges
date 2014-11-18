664e563b32da7e070759312a82a722dfeae2df62
Jacob Gillespie <jacobwgillespie@gmail.com>
2014-11-18 15:34:19 -0800

    Fix caching

--- web.rb
+++ web.rb
@@ -1,17 +1,18 @@
 require 'sinatra'
 require 'open-uri'
 require 'multi_xml'
+require 'digest/sha1'
 
 get '/:owner/:repo/:token' do
   url = "https://circleci.com/gh/#{params[:owner]}/#{params[:repo]}.cc.xml?circle-token=#{params[:token]}"
   xml = open(url).read
   status = MultiXml.parse(xml)['Projects']['Project']['lastBuildStatus']
   badge = status == 'Success' ? 'build-passing-brightgreen.svg' : 'build-failing-red.svg'
+  badge = "https://img.shields.io/badge/#{badge}"
 
-  headers \
-    'Pragma' => 'no-cache',
-    'Cache-Control' => 'no-cache',
-    'Age' => 0
+  etag Digest::SHA1.base64digest(badge)
+  cache_control :no_cache, :no_store
+  headers 'Content-Type' => 'text/svg+xml'
 
-  redirect "https://img.shields.io/badge/#{badge}"
+  open(badge).read
 end
